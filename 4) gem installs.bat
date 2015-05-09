d65959ffaadb976704fab5fb044cb2a9ed5c2599
Vincent Lui <vincent.lui@tpg.com.au>
2015-05-09 23:20:15 +1000

    Syntax error on gem installs

--- 4) gem installs.bat
+++ 4) gem installs.bat
@@ -1,12 +1,12 @@
 :: Workaround if you get an SSL error... Run the following commands manually
 :: http://stackoverflow.com/questions/10246023/bundle-install-fails-with-ssl-certificate-verification-error
-::gem sources -r https://rubygems.org/
-::gem sources -a http://rubygems.org/
-::gem update --system
-::gem sources -r http://rubygems.org/
-::gem sources -a https://rubygems.org/
+::call gem sources -r https://rubygems.org/
+::call gem sources -a http://rubygems.org/
+::call gem update --system
+::call gem sources -r http://rubygems.org/
+::call gem sources -a https://rubygems.org/
 
-call gem install sass
+::call gem install sass
 
 @echo off
 echo .
