87db9a532ad0e895f3e13a811454f97f9976a2db
Ben McCallum <ben.mccallum@live.com.au>
2016-01-02 17:41:35 +1100

    Update 2) choco installs.bat
    Commented out everything but the essentials, so people must opt in to their own requirements.
    Removed updater package as it's not for package consumers, rather package authors.

--- 2) choco installs.bat
+++ 2) choco installs.bat
@@ -1,112 +1,117 @@
-:: Security softare
-choco install -y avastfreeantivirus
-choco install -y avastbrowsercleanup
-
 :: Development tools
-choco install -y nodejs.install
-choco install -y ruby
-choco install -y fiddler
-::choco install -y westwindwebsurge
-::choco install -y compass
-::choco install -y scriptcs
-::choco install -y putty
-::choco install -y rdcman
-choco install -y smtp4dev
-::choco install -y yeoman
+cinst -y nodejs.install
+cinst -y ruby
+::cinst -y fiddler
+::cinst -y westwindwebsurge
+::cinst -y compass
+::cinst -y scriptcs
+::cinst -y putty
+::cinst -y rdcman
+::cinst -y yeoman
+:: cinst -y curl
+
+:: SMTP server mocks
+::cinst -y smtp4dev
 
 :: Source control
-::choco install -y git.install
-::choco install -y githubforwindows
-choco install -y sourcetree
+::cinst -y git.install
+::cinst -y githubforwindows
+::cinst -y sourcetree
 
 :: IDEs / text editors
-choco install -y notepadplusplus.install
-::choco install -y brackets
-::choco install -y sublimetext3
-::choco install -y webstorm
+::cinst -y vscode
+::cinst -y notepadplusplus.install
+::cinst -y brackets
+::cinst -y sublimetext3
+::cinst -y webstorm
+
+:: IIS
+::cinst -y urlrewrite
+
+:: Windows Installers
+::cinst -y webpi
 
 :: .NET development tools
-::choco install -y nuget.commandline
-::choco install -y nugetpackageexplorer
-choco install -y linqpad
-::choco install -y nugetpackagemanager
-::choco install -y typescript.vs
-::choco install -y webpi
-::choco install -y urlrewrite
-::choco install -y resharper
+::cinst -y linqpad
+::cinst -y nuget.commandline
+::cinst -y nugetpackageexplorer
+::cinst -y nugetpackagemanager
+::cinst -y typescript.vs
+::cinst -y resharper
 
 :: .NET reflection tools
-choco install -y ilspy
-choco install -y dotpeek
+::cinst -y dotpeek
+::cinst -y ilspy
 
 :: Merge tools
-::choco install -y beyondcompare
-::choco install -y winmerge
-choco install -y p4merge
+::cinst -y beyondcompare
+::cinst -y winmerge
+::cinst -y p4merge
 
 :: Databases
-::choco install -y mongodb
-choco install -y sqlserver2014express
+::cinst -y mongodb
+::cinst -y sqlserver2014express
 
 :: Browsers
-::choco install -y google-chrome-x64
-::choco install -y firefox
-::choco install -y opera
-::choco install -y googlechrome.canary
-::choco install -y maxthon
+::cinst -y google-chrome-x64
+::cinst -y firefox
+::cinst -y opera
+::cinst -y googlechrome.canary
+::cinst -y maxthon
 
 :: Media players and editors
-choco install -y spotify
-::choco install -y handbrake.install
-choco install -y vlc
-::choco install -y audacity
-::choco install -y itunes
-choco install -y kodi
+::cinst -y spotify
+::cinst -y handbrake.install
+::cinst -y vlc
+::cinst -y audacity
+::cinst -y itunes
+::cinst -y kodi
 
 :: Gaming
-::choco install -y steam
-::choco install -y unity
-::choco install -y unitywebplayer
+::cinst -y steam
+::cinst -y unity
+::cinst -y unitywebplayer
 
 :: Cloud storage
-::choco install -y dropbox
-::choco install -y googledrive
-::choco install -y onedrive
+::cinst -y dropbox
+::cinst -y googledrive
+::cinst -y onedrive
 
 :: Note taking
-::choco install -y evernote
+::cinst -y evernote
 
 :: Instant messaging / chat clients
-choco install -y skype
-::choco install -y hipchat
+::cinst -y skype
+::cinst -y hipchat
 
 :: Downloaders
-::choco install -y sabnzbd
+::cinst -y sabnzbd
+
+:: Security softare
+::cinst -y avastfreeantivirus
+::cinst -y avastbrowsercleanup
 
 :: Misc utils
-::choco install -y imgburn
-::choco install -y keepass.install
-::choco install -y keepass-keepasshttp
-::choco install -y 7zip.install
-choco install -y winrar
-::choco install -y clover
-::choco install -y f.lux
-::choco install -y linkshellextension
-::choco install -y windowslivewriter
-::choco install -y speedfan
-::choco install -y sysinternals
-::choco install -y ccleaner
-::choco install -y foxitreader
-choco install -y paint.net
-::choco install -y sublimetext3
-::choco install -y teamviewer
-
-:: Chocolatey win
-choco install -y chocolateypackageupdater -y
+::cinst -y imgburn
+::cinst -y keepass.install
+::cinst -y keepass-keepasshttp
+::cinst -y 7zip.install
+::cinst -y winrar
+::cinst -y clover
+::cinst -y f.lux
+::cinst -y linkshellextension
+::cinst -y windowslivewriter
+::cinst -y speedfan
+::cinst -y sysinternals
+::cinst -y ccleaner
+::cinst -y foxitreader
+::cinst -y paint.net
+::cinst -y sublimetext3
+::cinst -y teamviewer
 
 @echo off
 echo .
 echo .
 echo .
-echo Complete. Check output to see if all 'choco install' statements succeeded.
+echo Complete. Check output to see if all 'cinst' statements succeeded.
 pause
